b96abe11232d8ee8ab8b5ccffd587f2cdedeb4b0
Chris Libby <chris.a.libby@gmail.com>
2021-06-09 09:15:30 -0400

    commiting my changes

--- prtg2pd.ps1
+++ prtg2pd.ps1
@@ -2,9 +2,12 @@
 
 # Notes
 # Copy the line below (exluding the hash) into the parameters field in the PRTG notification template
-# '%probe' '%device' '%deviceid' '%sensor' '%sensorid' '%group' '%groupid' '%home' '%host' '%status' '%colorofstate' '%down' '%priority' '%message' '%comments' '%datetime'
+# '%probe' '%device' '%deviceid' '%sensor' '%sensorid' '%group' '%groupid' '%home' '%host' '%status' '%colorofstate' '%down' '%priority' '%message' '%comments' '%datetime' 'critical\warning\info'
 
 # Ingest the alert payload from PRTG
+
+# TODO Update README.md for the proper parameter usage.
+
 Param(
     [string]$probe,
     [string]$device,
@@ -21,29 +24,58 @@ Param(
     [string]$priority,
     [string]$message,
     [string]$comments,
-    [string]$datetime
+    [string]$datetime,
+	[string]$Severity
 )
 
-# Determine the Event Action
-$regex = [regex] "\((.*)\)"
-$action = $regex::match($status, $regex).groups[1]
+# TODO Change PD RoutingKey to passed arguement and not hardcoded.DESCRIPTION
+#  Moving this to a command line arguement will allow you to use one script
+#  to alert to multiple services in PagerDuty.  This will allow for flexability
+#  when alerting and with business service dependancies.
 
-switch ($action) {
-    "now: Up" { $PDevent = "resolve" }
+# ---------------------------------
+# PagerDuty API Routing Key and Url
+# ---------------------------------
+# Change the $RoutingKey field to the value set by the Event API v2 integration
+# settings for the PagerDuty service you wish to route alerts to.
+# DO NOT CHANGE THE URL.
+$RoutingKey = "3e94479ab3e14704c0a27ce59a8cbfb5"
+$Url = "https://events.pagerduty.com/v2/enqueue"
+# ---------------------------------
+
+# ---------------------------------
+# This code was commented out as it did not work for my purposes.
+# ---------------------------------
+# Determine the Event Action
+#$regex = [regex] "\((.*)\)"
+#$action = $regex::match($status, $regex).groups[1]
+
+#switch ($action) {
+#    "Up" { $PDevent = "resolve" }
+#    default { $PDevent = "trigger" }
+#}
+
+# ---------------------------------
+# Determine if you should open, acknowledge, or resolve this incident.
+# ---------------------------------
+switch ($status) {
+    "Up" { $PDevent = "resolve" }
+    "Down (Acknowledged)" { $PDevent = "acknowledge" }
+    "Paused" { $PDevent = "acknowledge" }
     default { $PDevent = "trigger" }
 }
 
+# ---------------------------------
+# This code was commented out as it did not work for my purposes.
+# ---------------------------------
 # Determine the Severity
-switch ($colorofstate) {
-    "#b4cc38"	{ $Severity = "info" }
-    "#ffcb05"	{ $Severity = "warning" }
-    #   "Error"		{$Severity="error"}
-    "#d71920"	{ $Severity = "critical" }
-    default { $Severity = "critical" }
-}
-
-$RoutingKey = "3534011a3b6d450a8ae967b57f6ef48a"
-$Url = "https://events.pagerduty.com/v2/enqueue"
+#switch ($colorofstate) {
+#    "#b4cc38"	{ $Severity = "info" }
+#    "#ffcb05"	{ $Severity = "warning" }
+#    #   "Error"		{$Severity="error"}
+#    "#d71920"	{ $Severity = "critical" }
+#    default { $Severity = "critical" }
+#}
 
 $Description = "$device $sensor $status $down"
 # $Severity = "critical"
@@ -52,7 +84,7 @@ $Timestamp = Get-Date -UFormat "%Y-%m-%dT%T%Z"
 $AlertPayload = @{
     routing_key  = $RoutingKey
     event_action = $PDevent
-    dedup_key    = $sensor
+    dedup_key    = "$deviceid-$sensorid"
     client       = "PRTG Network Monitor"
     client_url   = $prtg_home
     payload      = @{
@@ -87,10 +119,15 @@ $json = ConvertTo-Json -InputObject $AlertPayload
 
 $logEvents = "C:\pagerduty\logs\prtg2pd_log.txt"
 
+Add-Content -Path $logEvents -Value $Url
+Add-Content -Path $logEvents -Value $json
+
 # Send to PagerDuty and Log Results
 
 $LogMtx = New-Object System.Threading.Mutex($False, "LogMtx")
 $LogMtx.WaitOne() | Out-Null
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
 
 try {
     Invoke-RestMethod	-Method Post `
